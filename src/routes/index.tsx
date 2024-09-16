58bbb9715bf0a8e0c84925a390b7ee8df9c1676a
Mathias Johannsen <mathias.j@arkulpa.at>
2024-09-16 16:35:35 +0200

    hours minutes seconds

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,45 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   const time = new Date();
-  const seconds = (time.getTime() % (1000 * 60)) / 1000;
+  const seconds = time.getSeconds() + time.getMilliseconds() / 1000;
+  const minutes = time.getMinutes() + seconds / 60;
+  const hours = time.getHours() + minutes / 60;
+
   return (
-    <div class="max-w-2xl mx-auto">
+    <div class="max-w-4xl mx-auto">
       <h1 class="text-3xl text-center font-bold mb-8">Jahresuhr</h1>
-      <div class="flex justify-center w-full">
-        <div style={{ transform: `rotate(${seconds * 6}deg)` }}>
+      <div class="mx-auto relative h-[500px] w-[500px] border">
+        <div
+          class="absolute inset-0 p-0 w-full h-full"
+          style={{ transform: `rotate(${hours * 30}deg)` }}
+        >
           <div
-            class="h-96 w-96 animate-spin border-4 rounded-full flex justify-center"
+            class="h-full w-full animate-spin border-4 bg-white rounded-full flex justify-center"
+            style={{ animationDuration: "43200s" }}
+          >
+            <div class="bg-black w-0.5 h-1/2"></div>
+          </div>
+        </div>
+        <div
+          class="absolute inset-0 p-12 w-full h-full"
+          style={{ transform: `rotate(${minutes * 6}deg)` }}
+        >
+          <div
+            class="h-full w-full animate-spin border-4 bg-white rounded-full flex justify-center"
+            style={{ animationDuration: "3600s" }}
+          >
+            <div class="bg-black w-0.5 h-1/2"></div>
+          </div>
+        </div>
+        <div
+          class="absolute inset-0 p-24 w-full h-full"
+          style={{ transform: `rotate(${seconds * 6}deg)` }}
+        >
+          <div
+            class="h-full w-full animate-spin border-4 bg-white rounded-full flex justify-center"
             style={{ animationDuration: "60s" }}
           >
-            <div class="bg-black w-2 h-48"></div>
+            <div class="bg-black w-0.5 h-1/2"></div>
           </div>
         </div>
       </div>
